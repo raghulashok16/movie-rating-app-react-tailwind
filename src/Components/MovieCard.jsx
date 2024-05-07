62eb424bb353cf19f8ed51dc8266e5f577e31cd7
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-05-07 23:00:53 +0530

    added api key for tmdb

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+    const { id, title, overview, vote_average, release_date, poster_path } = movie;
+    // console.log(id, title, overview, vote_average, release_date)
     return (
         <div className='w-[10rem] m-9 hover:scale-110 duration-200 '>
             <a href="" className='relative group'>
-                <img src="https://marketplace.canva.com/EAFTl0ixW_k/1/0/1131w/canva-black-white-minimal-alone-movie-poster-YZ-0GJ13Nc8.jpg" alt="" />
-                <div className='absolute bottom-0 p-2 group-hover:bg-gradient-to-t from-black from-15% ... opacity-0 group-hover:opacity-100 duration-200 '>
-                    <h3 className='text-xs'>Movie Name</h3>
+                <img src={`https://image.tmdb.org/t/p/w185${poster_path}`} alt="" />
+                <div className='absolute bottom-0 p-2 group-hover:bg-gradient-to-t from-black from-40% ... opacity-0 group-hover:opacity-100 duration-200 '>
+                    <h3 className='text-xs font-extrabold text-yellow-300'>{title}</h3>
                     <div className='flex flex-row justify-between'>
-                        <p className='text-ssss text-yellow-300'>10-20-2020</p>
-                        <p className='text-ssss text-yellow-300'>8.0</p>
+                        <p className='text-ssss text-yellow-300'>{release_date}</p>
+                        <p className='text-ssss text-yellow-300'>{vote_average.toFixed(1)}</p>
                     </div>
-                    <p className='text-ss mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione delectus a ducimus in magnam est!</p>
+                    <p className='text-ss mt-1 line-clamp-2'>{overview}</p>
                 </div>
             </a>
         </div>
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Fire from "../assets/fire.svg";
 
 import MovieCard from './MovieCard';
 import { data } from 'autoprefixer';
 const MovieList = () => {
-    useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=c7548debf7c3c70ef38c0a1041ef40c5')
-            .then(res => res.json())
-            .then((data) => console.log(data))
-    }, [])
+    const [movies, setMovies] = useState([]);
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [rating, setRating] = useState(0);
+
+    useEffect(() => { fetchMovies() }, []);
+
+    const fetchMovies = async () => {
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=c7548debf7c3c70ef38c0a1041ef40c5');
+        const data = await response.json();
+        setMovies(data.results);
+        setFilteredMovies(data.results);
+    };
+
+    const handleRating = (rate) => {
+        if (rate !== rating) {
+            const newMovieList = movies.filter(movie => movie.vote_average > rate);
+            setFilteredMovies(newMovieList);
+            setRating(rate);
+        } else {
+            setFilteredMovies(movies);
+            setRating(0);
+        }
+
+    }
+
     return (
         <>
             <header className='flex px-5'>
                 <h3>Popular <img src={Fire} className='w-4 pb-1  inline mr-4' alt="" /></h3>
-                <div className='ms-auto flex'>
+                <div className='ms-auto flex select-none'>
                     <ul className='flex flex-row '>
-                        <li className='pr-2'>8+ Star</li>
-                        <li className='pr-2'>7+ Star</li>
-                        <li className='pr-4'>6+ Star</li>
+                        <li className={`pr-2 cursor-pointer ${rating === 8 && 'underline'}`} onClick={() => handleRating(8)}>8+ Star</li>
+                        <li className={`pr-2 cursor-pointer ${rating === 7 && 'underline'}`} onClick={() => handleRating(7)}>7+ Star</li>
+                        <li className={`pr-2 cursor-pointer ${rating === 6 && 'underline'}`} onClick={() => handleRating(6)}>6+ Star</li>
                     </ul>
                     <select name="SortBy" id="" className='pr-2  bg-black'>
                         <option value="">SortBy</option>
@@ -31,15 +51,9 @@ const MovieList = () => {
                 </div>
             </header>
             <div className='flex justify-evenly flex-wrap mt-9'>
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {
+                    filteredMovies.map(movie => (<MovieCard key={movie.id} movie={movie} />))
+                }
             </div>
         </>
     )
